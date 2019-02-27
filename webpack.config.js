1fad13a8f72aa6658c563a3302a08968f813b810
Kevin Destrem <kefniark@gmail.com>
2019-02-27 19:26:00 +0900

    Ignore doc in dev mode + fix mobile double tap issue in samples

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 
-module.exports = {
-	entry: './src/library/index.ts',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'jil.min.js',
-		library: 'jil',
-		//libraryExport: 'default',
-		libraryTarget: 'umd',
-		umdNamedDefine: true
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"]
-	},
-	module: {
-		rules: [
-			{ test: /\.tsx?$/, loader: "ts-loader" }
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ filename: 'samples/index.html', template: 'src/samples/index.html' }),
-		// new CopyPlugin([
-		// 	{ from: 'src/samples/panel.html', to: 'samples' },
-		// 	{ from: 'src/samples/scene.html', to: 'samples' }
-		// ]),
-		new TypedocWebpackPlugin({
+module.exports = (env, argv) => {
+
+	const plugins = [
+		new HtmlWebpackPlugin({ filename: 'samples/index.html', template: 'src/samples/index.html' })
+	];
+
+	// generate the doc only during build (not in dev)
+	if (argv.mode === 'production') {
+		plugins.push(new TypedocWebpackPlugin({
 			name: 'Jil',
 			theme: 'minimal',
 			out: './docs',
@@ -35,11 +18,32 @@ module.exports = {
 			exclude: ['src/library/behaviours/**/*', 'src/library/helpers/*'],
 			excludePrivate: true,
 			excludeProtected: true
-		}, './src/library')
-	],
-	mode: "development",
-	devServer: {
-		contentBase: path.join(__dirname, 'dist', 'samples'),
-		port: 80
+		}, './src/library'));
 	}
-};
+
+	// normal webpack config
+	return {
+		entry: './src/library/index.ts',
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'jil.min.js',
+			library: 'jil',
+			libraryTarget: 'umd',
+			umdNamedDefine: true
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		module: {
+			rules: [
+				{ test: /\.tsx?$/, loader: "ts-loader" }
+			]
+		},
+		plugins,
+		mode: "development",
+		devServer: {
+			contentBase: path.join(__dirname, 'dist', 'samples'),
+			port: 80
+		}
+	};
+}
